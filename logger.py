1decf4bb6c692d33c5935e602230cd9cdbc8e05e
ncomputers <ncomputerudr@gmail.com>
2025-04-10 18:51:30 +0530

    Update logger.py

--- logger.py
+++ logger.py
@@ -1,21 +1,26 @@
 import logging
-import config
 
 def setup_logging():
     logger = logging.getLogger()
     logger.setLevel(logging.DEBUG)
+
     if logger.hasHandlers():
         logger.handlers.clear()
-    file_handler = logging.FileHandler(config.LOG_FILE)
-    file_handler.setLevel(logging.DEBUG)
-    file_formatter = logging.Formatter('%(asctime)s %(levelname)s: %(message)s')
-    file_handler.setFormatter(file_formatter)
+
+    # Remove this block to prevent file logging
+    # file_handler = logging.FileHandler(config.LOG_FILE)
+    # file_handler.setLevel(logging.DEBUG)
+    # file_formatter = logging.Formatter('%(asctime)s %(levelname)s: %(message)s')
+    # file_handler.setFormatter(file_formatter)
+    # logger.addHandler(file_handler)
+
+    # Only log to console
     console_handler = logging.StreamHandler()
     console_handler.setLevel(logging.INFO)
     console_formatter = logging.Formatter('%(asctime)s %(levelname)s: %(message)s')
     console_handler.setFormatter(console_formatter)
-    logger.addHandler(file_handler)
     logger.addHandler(console_handler)
+
     return logger
 
 if __name__ == "__main__":
